a7e192542b6cd8fed7568ab8dc6c0e4435060610
mesu-ai <mesu.s.ss@gmail.com>
2021-11-15 19:01:18 +0600

    myreview

---
+++ src/hooks/useMyreview.js
@@ -0,0 +1,23 @@
+import  { useEffect, useState } from 'react';
+import useAuth from './useAuth';
+
+const useMyreview = () => {
+    const {user}= useAuth();
+
+    const [reviewMyself,setReviewMyself]=useState([]);
+    const url=`http://localhost:5000/reviews?email=${user.email}`;
+    
+    useEffect(()=>{
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>{
+           
+            setReviewMyself(data);
+        })
+
+    },[url, user.email]);
+    
+    return {reviewMyself,setReviewMyself};
+};
+
+export default useMyreview;
