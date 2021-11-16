4d8ada5d014ca63ffbd5206b0e44e0c61df2c9eb
mesu-ai <mesu.s.ss@gmail.com>
2021-11-16 20:58:05 +0600

    update

--- src/pages/dashboard/dashboardHome/DashboardHome.js
+++ src/pages/dashboard/dashboardHome/DashboardHome.js
@@ -7,7 +7,7 @@ const DashboardHome = () => {
         <div style={{marginTop:'-70px',backgroundColor:'#cfd8dc',padding:'15px'}}>
             <img src={cover} alt="" width="100%" height="100%"></img>
             
-            <Button  sx={{ mt: { xs:'none',sm:'none', lg: -13 } }} color="success" variant='contained' >Register Free &gt;&gt;</Button>
+            <Button  sx={{ mt: { xs:'none',sm:'none', lg: -13 },fontSize:{ xs:'2vw',sm:'2vw', md:'1vw' } }} color="error" variant='contained' >Register Free &gt;&gt;</Button>
             
             
             
